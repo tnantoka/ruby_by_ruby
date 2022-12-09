4fc51515faebd7988f940f3d913154195ee0b785
tnantoka <tnantoka@bornneet.com>
2022-12-09 21:53:36 +0900

    feat: bundle gem -b --linter=rubocop --mit --no-coc ruby_by_ruby

---
+++ lib/ruby_by_ruby.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ruby_by_ruby/version"
+
+module RubyByRuby
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ruby_by_ruby/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module RubyByRuby
+  VERSION = "0.1.0"
+end
